a6436fa613a6a5c40b4942aa712e9ef302eb1baf
Katherine Evans <katherine@launchpadlab.com>
2021-12-23 18:03:54 -0600

    updates to cute logos

--- src/components/CuteLogos.vue
+++ src/components/CuteLogos.vue
@@ -5,8 +5,26 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+  data () {
+    return {
+      data: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://127.0.0.1:3000/api/v1/logos')
+      .then(response => {
+        this.data = response
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+  } 
 }
 </script>
 
